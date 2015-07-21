a725444b60e689367161347ea3fb0aac788bcf48
Sava Chankov <sava@tutuf.com>
2015-07-21 14:53:09 +0300

    Revert "Change the view of the TODO version from vertical to horizontal (refs…"

--- app/helpers/simple_kanban_helper.rb
+++ app/helpers/simple_kanban_helper.rb
@@ -8,12 +8,4 @@ module SimpleKanbanHelper
     when 'Поддръжка' then '#628DB6'
     end
   end
-
-  def issues_categories_todo
-    Version.find_by_name('TODO').fixed_issues.map{ |issue| issue.category }.uniq
-  end
-
-  def issues_by_category(category)
-    Version.find_by_name('TODO').fixed_issues.select{ |issue| issue.category == category}
-  end
 end
